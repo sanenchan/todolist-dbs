6a992212bc692aeb9ed37a1c6c543f038c28beeb
Santoso <33777569+sanenchan@users.noreply.github.com>
2024-05-21 18:42:48 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './components/Header';
+import TodoList from './components/Todolist';
+import { useState } from "react";
 function App() {
+  const [isRefresh, setIsRefresh] = useState(true)
+
+  const setRefresh = (status) => {
+    setIsRefresh(status)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="App">
+      <div className="content">
+        <Header setRefresh={setRefresh} />
+        <TodoList setRefresh={setRefresh} isRefresh={isRefresh} />
+      </div>
     </div>
   );
 }
